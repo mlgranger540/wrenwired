8a7839ebd4b76bd325559779c7289e3dbb97eb76
mlgranger540 <mattgranger540@outlook.com>
2023-03-01 00:06:36 +0000

    Refactor index.js to check for computed style values for display and declare variables on page load

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,45 +1,61 @@
+let startingPointN;
+let startingPointE;
+let startingPointS;
+let startingPointW;
+
+window.onload = (event) =>{
+    startingPointN = document.getElementById("starting-point-n-img");
+    console.log("Loaded starting point N: " + startingPointN);
+    startingPointE = document.getElementById("starting-point-e-img");
+    console.log("Loaded starting point E: " + startingPointE);
+    startingPointS = document.getElementById("starting-point-s-img");
+    console.log("Loaded starting point S: " + startingPointS);
+    startingPointW = document.getElementById("starting-point-w-img");
+    console.log("Loaded starting point W: " + startingPointW);
+}
+
 function cycleRight() {
-    let startingPointN = document.getElementById("starting-point-n-img");
-    let startingPointE = document.getElementById("starting-point-e-img");
-    let startingPointS = document.getElementById("starting-point-s-img");
-    let startingPointW = document.getElementById("starting-point-w-img");
-    if (startingPointN.style.display === "block") {
+    const compStylesN = window.getComputedStyle(startingPointN);
+    const compStylesE = window.getComputedStyle(startingPointE);
+    const compStylesS = window.getComputedStyle(startingPointS);
+    if (compStylesN.getPropertyValue('display') === "block") {
         startingPointN.style.display = "none";
         startingPointE.style.display = "block";
-    } else if (startingPointE.style.display === "block") {
+        console.log("Turned from north to east");
+    } else if (compStylesE.getPropertyValue('display') === "block") {
         startingPointE.style.display = "none";
         startingPointS.style.display = "block";
-    } else if (startingPointS.style.display === "block") {
+        console.log("Turned from east to south");
+    } else if (compStylesS.getPropertyValue('display') === "block") {
         startingPointS.style.display = "none";
         startingPointW.style.display = "block";
-    } else if (startingPointW.style.display === "block") {
-        startingPointW.style.display = "none";
-        startingPointN.style.display = "block";
+        console.log("Turned from south to west");
     } else {
         startingPointW.style.display = "none";
         startingPointN.style.display = "block";
+        console.log("Turned from west to north");
     }
 }
 
 function cycleLeft() {
-    let startingPointN = document.getElementById("starting-point-n-img");
-    let startingPointE = document.getElementById("starting-point-e-img");
-    let startingPointS = document.getElementById("starting-point-s-img");
-    let startingPointW = document.getElementById("starting-point-w-img");
-    if (startingPointN.style.display === "block") {
+    const compStylesN = window.getComputedStyle(startingPointN);
+    const compStylesW = window.getComputedStyle(startingPointW);
+    const compStylesS = window.getComputedStyle(startingPointS);
+    if (compStylesN.getPropertyValue('display') === "block") {
         startingPointN.style.display = "none";
         startingPointW.style.display = "block";
-    } else if (startingPointW.style.display === "block") {
+        console.log("Turned from north to west");
+    } else if (compStylesW.getPropertyValue('display') === "block") {
         startingPointW.style.display = "none";
         startingPointS.style.display = "block";
-    } else if (startingPointS.style.display === "block") {
+        console.log("Turned from west to south");
+    } else if (compStylesS.getPropertyValue('display') === "block") {
         startingPointS.style.display = "none";
         startingPointE.style.display = "block";
-    } else if (startingPointE.style.display === "block") {
+        console.log("Turned from south to east");
+    } else {
         startingPointE.style.display = "none";
         startingPointN.style.display = "block";
-    } else {
-        startingPointW.style.display = "none";
-        startingPointN.style.display = "block";
+        console.log("Turned from east to north");
     }
 }
