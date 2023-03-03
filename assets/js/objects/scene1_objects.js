7c522a9352d21a04c0b33cdcc8da139b5a335004
mlgranger540 <mattgranger540@outlook.com>
2023-03-03 22:10:15 +0000

    Replace placeholder images with original images

--- assets/js/objects/scene1_objects.js
+++ assets/js/objects/scene1_objects.js
@@ -2,7 +2,7 @@ const storyImgs = {
     startingPointNImg: {
         left: "turn-left-4",
         right: "turn-right-4",
-        src: "assets/images/starting-point-n-ph.jpeg",
+        src: "assets/images/starting-point-n.png",
         alt: "An empty field - north",
         title: "The field stretches out to the north",
         next_left: "turn-left-1",
@@ -11,7 +11,7 @@ const storyImgs = {
     startingPointEImg: {
         left: "turn-left-3",
         right: "turn-right-1",
-        src: "assets/images/starting-point-e-ph.jpeg",
+        src: "assets/images/starting-point-e.png",
         alt: "An empty field - east",
         title: "The field stretches out to the east",
         next_left: "turn-left-4",
@@ -20,7 +20,7 @@ const storyImgs = {
     startingPointSImg: {
         left: "turn-left-2",
         right: "turn-right-2",
-        src: "assets/images/starting-point-s-ph.jpeg",
+        src: "assets/images/starting-point-s.png",
         alt: "An empty field - south",
         title: "The field stretches out to the south",
         next_left: "turn-left-3",
@@ -29,7 +29,7 @@ const storyImgs = {
     startingPointWImg: {
         left: "turn-left-1",
         right: "turn-right-3",
-        src: "assets/images/starting-point-w-ph.jpeg",
+        src: "assets/images/starting-point-w.png",
         alt: "An empty field - west",
         title: "The field stretches out to the west",
         next_left: "turn-left-2",
